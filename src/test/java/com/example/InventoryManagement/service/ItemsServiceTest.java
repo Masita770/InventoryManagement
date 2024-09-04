c6a33bbcbb44c0d518c6306a9ccc678f0fdfb978
Masita770 <ozon777ym12@gmail.com>
2024-09-04 23:32:50 +0900

    fix: ItemsController.java 修正

---
+++ src/test/java/com/example/InventoryManagement/service/ItemsServiceTest.java
@@ -0,0 +1,9 @@
+package com.example.InventoryManagement.service;
+
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ItemsServiceTest {
+}
