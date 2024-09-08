9e8eebe0e983fe1a959d25ab90d7d9df5f4be6b5
Masita770 <ozon777ym12@gmail.com>
2024-09-08 23:51:17 +0900

    fix: Items.java build.gradle 修正

--- src/main/java/com/example/InventoryManagement/domain/Items.java
+++ src/main/java/com/example/InventoryManagement/domain/Items.java
@@ -6,6 +6,14 @@ public class Items {
     private int id;
     private String category;
     private String item;
-    private Timestamp registrationDate;
-    private Timestamp deletedDate;
+    private String registrationDate;
+    private String deletedDate;
+
+    public Items(int id, String category, String item, String registrationDate, String deletedDate) {
+        this.id = id;
+        this.category = category;
+        this.item = item;
+        this.registrationDate = registrationDate;
+        this.deletedDate = deletedDate;
+    }
 }
