bf930f3723dfb066a0b3d60852862f1929be8324
Masita770 <ozon777ym12@gmail.com>
2024-09-22 00:01:17 +0900

    fix: Items.java 修正

--- src/main/java/com/example/InventoryManagement/domain/Items.java
+++ src/main/java/com/example/InventoryManagement/domain/Items.java
@@ -10,10 +10,10 @@ public class Items {
     private int id;
     private String category;
     private String item;
-    private int registrationDate;
-    private int deletedDate;
+    private String registrationDate;
+    private String deletedDate;
 
-    public Items(int id,String category, String item, int registrationDate, int deletedDate) {
+    public Items(int id,String category, String item, String registrationDate, String deletedDate) {
         this.id = id;
         this.category = category;
         this.item = item;
