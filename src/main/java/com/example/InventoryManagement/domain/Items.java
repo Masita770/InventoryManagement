7f10cc1c3d0644328dbeff1563b8ccff60ac73d2
Masita770 <ozon777ym12@gmail.com>
2024-10-19 23:58:12 +0900

    fix: ItemsController.java 修正

--- src/main/java/com/example/InventoryManagement/domain/Items.java
+++ src/main/java/com/example/InventoryManagement/domain/Items.java
@@ -16,12 +16,13 @@ public class Items {
     Stocks stocksList;
     Orders ordersList;
 
-    public Items(int id,String category, String item, String registrationDate, String deletedDate, Stocks stocksList) {
+    public Items(int id,String category, String item, String registrationDate, String deletedDate, Stocks stocksList, Orders ordersList) {
         this.id = id;
         this.category = category;
         this.item = item;
         this.registrationDate = registrationDate;
         this.deletedDate = deletedDate;
         this.stocksList = stocksList;
+        this.ordersList = ordersList;
     }
 }
