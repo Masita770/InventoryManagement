21f67425d01f01578b77717ed027dc63482ef540
Masita770 <ozon777ym12@gmail.com>
2025-02-23 14:22:49 +0900

    fix: ItemsMapperTest.java 追加 add: dbunit.yml tests.yml tests_expected.yml 追加

--- src/test/java/com/example/InventoryManagement/mapper/ItemsMapperTest.java
+++ src/test/java/com/example/InventoryManagement/mapper/ItemsMapperTest.java
@@ -2,16 +2,15 @@ package com.example.InventoryManagement.mapper;
 
 
 import com.example.InventoryManagement.domain.Items;
-import com.example.InventoryManagement.domain.Stocks;
 import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
+import com.github.database.rider.spring.api.DBRider;
 import org.assertj.core.api.Assertions;
-import org.assertj.core.api.ListAssert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +18,7 @@ import java.util.List;
 import java.util.Optional;
 
 @MybatisTest
+@DBRider
 @ExtendWith(SpringExtension.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 //@ExtendWith(MockitoExtension.class)
@@ -32,15 +32,15 @@ class ItemsMapperTest {
 
 
     @Test
-    @DataSet(value = "com/example/InventoryManagement/ItemsMapperTest.yml")
-    @Transactional
+    @DataSet(value = "dataSets/tests.yml")
     void 全件一覧取得できるか検証() {
-        List<Items> sut = itemsMapper.itemsSelectAll();
-//        List<Items> itemsList = Assertions.assertThat(sut).hasSize(3).contains(
-//                new Items(1, "机", "オフィスデスク", "0000-00-00 00:00:00", "0000-00-00-00-00-0000"),
-//                new Items(2, "机", "オフィスデスク", "0000-00-00 00:00:00", "0000-00-00-00-00-0000"),
-//                new Items(3, "机", "ゲーミングデスク", "2024-09-16 22:36:15", "0000-00-00-00-0000")
-//        );
+        List<Items> itemsList = itemsMapper.itemsSelectAll();
+//        Assertions.assertThat(itemsList).hasSize(3);
+        Assertions.assertThat(itemsList).containsExactly(
+                new Items(1, "机", "オフィスデスク", "0000-00-00 00:00:00", "0000-00-00 00:00:00"),
+                new Items(2, "机", "キッチンテーブル", "0000-00-00 00:00:00", "0000-00-00 00:00:00"),
+                new Items(3, "机", "ゲーミングデスク", "2024-09-16 22:36:15", "0000-00-00 00:00:00")
+        );
     }
 
     @Test
