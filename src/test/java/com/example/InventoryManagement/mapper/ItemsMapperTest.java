a9db9df7b76dfaddcec602c28fcfb1a74121bd4b
Masita770 <ozon777ym12@gmail.com>
2025-03-21 00:01:27 +0900

    add: schema.sql data.sql 追加 fix: ItemsMapperTest.java index. 修正

--- src/test/java/com/example/InventoryManagement/mapper/ItemsMapperTest.java
+++ src/test/java/com/example/InventoryManagement/mapper/ItemsMapperTest.java
@@ -2,8 +2,11 @@ package com.example.InventoryManagement.mapper;
 
 
 import com.example.InventoryManagement.domain.Items;
+import com.example.InventoryManagement.domain.Orders;
+import com.example.InventoryManagement.domain.Stocks;
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.spring.api.DBRider;
+import jakarta.validation.constraints.Max;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -42,6 +45,12 @@ class ItemsMapperTest {
 //                new Items(2, "机", "キッチンテーブル", "2025-03-12 23:43:21", "0000-00-00 00:00:00"),
 //                new Items(3, "机", "ゲーミングデスク", "2025-03-12 23:43:31", "0000-00-00 00:00:00")
 //        );
+        List<Stocks> stocksList = List.of(
+                new Stocks(1, 1, 5, "0000-00-00 00:00:00")
+        );
+        List<Orders> ordersList = List.of(
+                new Orders(1, 1, 5, "0000-00 00:00:00")
+        );
     }
 
     @Test
@@ -53,5 +62,15 @@ class ItemsMapperTest {
         Assertions.assertThat(selectOne).isEqualTo(Optional.of(
 //                new Items()));
                 new Items(1, "机", "オフィスデスク", "0000-00-00 00:00:00", "0000-00-00 00:00:00")));
+    }
+
+    @Test
+    void insertテスト() {
+        Orders orders = new Orders();
+        orders.setItemsId(1);
+        orders.setOrderDate(5);
+        itemsMapper.orderAdd(orders);
+        //TODO: 注文データを確認する機能を付ける必要性
+//        Orders orders1 = itemsMapper.itemsSelectOne(orders.getOrderId()).orElse(null);
     }
 }
