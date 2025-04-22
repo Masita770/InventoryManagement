5ce5be3d0db110eaefb93350034869e0d6cda8fc
Masita770 <ozon777ym12@gmail.com>
2025-04-23 00:07:02 +0900

    fix: OrderRequestForm.java 修正

---
+++ src/main/java/com/example/InventoryManagement/form/OrderRequestForm.java
@@ -0,0 +1,20 @@
+package com.example.InventoryManagement.form;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class OrderRequestForm {
+
+    private int ordersId;
+    private int itemsId;
+    @Pattern(regexp = "^[0-9]*$")
+    @NotNull
+    private int orderDate;
+    private String deliveryDate;
+}
