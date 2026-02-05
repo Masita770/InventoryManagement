89562099187e0fda81c2d348cd2e3930b551904e
Masita770 <ozon777ym12@gmail.com>
2026-02-06 00:08:21 +0900

    fix: ItemsServiceTest.java 修正

--- src/test/java/com/example/InventoryManagement/service/ItemsServiceTest.java
+++ src/test/java/com/example/InventoryManagement/service/ItemsServiceTest.java
@@ -3,6 +3,7 @@ package com.example.InventoryManagement.service;
 
 import com.example.InventoryManagement.domain.Items;
 import com.example.InventoryManagement.domain.Orders;
+import com.example.InventoryManagement.domain.Stocks;
 import com.example.InventoryManagement.mapper.ItemsMapper;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,7 @@ import static org.mockito.Mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 
@@ -38,6 +40,22 @@ public class ItemsServiceTest {
         List<Items> actual = itemsService.getSelectAll();
         Assertions.assertThat(actual).isEqualTo(itemsList);
         verify(itemsMapper, times(1)).itemsSelectAll();
+    }
+
+    @Test
+    void 商品選択と注文データを取得() {
+        Optional<Items> i = Optional.of(
+                new Items(1, "机", "オフィスデスク", "0000-00-00 00:00:00","FALSE",  List.of(new Stocks()), List.of(new Orders(47, 1, 20, "2025-11-12 23:48:00"),
+                        new Orders(48, 1, 20, "2025-11-17 20:08:37"),
+                        new Orders(49, 1, 20, "2025-11-17 22:22:52"),
+                        new Orders(50, 1, 20, "2025-11-30 19:54:16"),
+                        new Orders(64, 1, 3, "2025-12-18 22:56:14")))
+        );
+        doReturn(i).when(itemsMapper).itemsSelectOne(1);
+        Optional<Items> actual = itemsService.getSelectOne(1);
+        Assertions.assertThat(actual).isEqualTo(i);
+        verify(itemsMapper, times(1)).itemsSelectOne(1);
+//        Optional<Items> i = itemsMapper.itemsSelectOne(1);
     }
 
     // TODO 削除処理のテストでitemsMapperが呼び出されない理由を調べる
